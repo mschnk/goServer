fba5bbdc08d148552b1cdd4a489453ec389243bb
mschnk <marcelschenk@hotmail.com>
2024-03-08 22:24:23 +0100

    adding more structure

--- main.go
+++ main.go
@@ -1,31 +1,52 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
+
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
+	db := ConectaComBancoDeDados()
+	defer db.Close()
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8080", nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	produtos := []Produto{
-		{Nome: "Camiseta", Descricao: "Azul, bem bonita", Preco: 39, Quantidade: 5},
-		{"Tenis", "Confortável", 89, 3},
-		{"Fone", "Muito bom", 59, 2},
-		{"Produto novo", "Muito legal", 1.99, 1},
+	db := ConectaComBancoDeDados()
+	selectDeTodosOsProdutos, err := db.Query("select * from produtos")
+	if err != nil {
+		panic(err.Error())
+	}
+	p := Produto{}
+	produtos := []Produto{}
+
+	for selectDeTodosOsProdutos.Next() {
+		//declarando variaveis para receber os valores do banco depois
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+		//passando os valores do banco para as variaveis, caso nao tenha valores, ele retorna um erro
+		err = selectDeTodosOsProdutos.Scan(&id, &nome, &descricao, &preco, &quantidade)
+		if err != nil {
+			panic(err.Error())
+		}
+		//passando os valores das variaveis para a struct de p: Produto
+		p.Nome = nome
+		p.Descricao = descricao
+		p.Preco = preco
+		p.Quantidade = quantidade
+		//adicionando a struct p na lista de produtos
+		produtos = append(produtos, p)
 	}
 
 	temp.ExecuteTemplate(w, "index", produtos)
+	defer db.Close()
 }
 
-type Produto struct {
-	Nome       string
-	Descricao  string
-	Preco      float64
-	Quantidade int
-}
---
+++ models/produtos.go
@@ -0,0 +1,43 @@
+package models
+
+import (
+	"github.com/mschnk/goServer/db"
+	_ "github.com/lib/pq")
+
+type Produto struct {
+	Nome       string
+	Descricao  string
+	Preco      float64
+	Quantidade int
+}
+
+func BuscaTodosOsProdutos() []Produto {
+		db := ConectaComBancoDeDados()
+		selectDeTodosOsProdutos, err := db.Query("select * from produtos")
+		if err != nil {
+			panic(err.Error())
+		}
+		p := Produto{}
+		produtos := []Produto{}
+	
+		for selectDeTodosOsProdutos.Next() {
+			//declarando variaveis para receber os valores do banco depois
+			var id, quantidade int
+			var nome, descricao string
+			var preco float64
+			//passando os valores do banco para as variaveis, caso nao tenha valores, ele retorna um erro
+			err = selectDeTodosOsProdutos.Scan(&id, &nome, &descricao, &preco, &quantidade)
+			if err != nil {
+				panic(err.Error())
+			}
+			//passando os valores das variaveis para a struct de p: Produto
+			p.Nome = nome
+			p.Descricao = descricao
+			p.Preco = preco
+			p.Quantidade = quantidade
+			//adicionando a struct p na lista de produtos
+			produtos = append(produtos, p)
+		}
+		defer db.Close()
+		return produtos
+}
